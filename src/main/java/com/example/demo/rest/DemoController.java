6ae347ec982d85d98495c6f9ca3bd719ad34b0ca
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-06-02 00:53:54 +0530

    @Primary Specifier

--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
