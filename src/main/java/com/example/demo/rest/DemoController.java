ba1f2ed4f6fb7b112812ac1bebc7374eb398b182
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-06-02 00:37:17 +0530

    Class Qualifiers

--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.demo.rest;
 
 import com.example.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,12 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach){
-        myCoach=theCoach;
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+        myCoach = theCoach;
     }
 
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
 }
