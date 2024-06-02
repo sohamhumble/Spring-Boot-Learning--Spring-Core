7f40610c1dd95594172fc7e096bba682dffed262
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-06-02 12:37:31 +0530

    bean lifecycle: use @PostConstruct and @Preconstruct

--- src/main/java/com/example/demo/common/CricketCoach.java
+++ src/main/java/com/example/demo/common/CricketCoach.java
@@ -1,6 +1,10 @@
 package com.example.demo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class CricketCoach implements Coach {
@@ -8,5 +12,15 @@ public class CricketCoach implements Coach {
     public String getDailyWorkout() {
         System.out.println(getClass().getSimpleName());
         return "Practise fast bowling for 20 Minutes";
+    }
+
+    @PostConstruct
+    public void postCreation() {
+        System.out.println(getClass().getSimpleName() + " created on " + LocalDateTime.now());
+    }
+
+    @PreDestroy
+    public void predestroy() {
+        System.out.println(getClass().getSimpleName() + " destoryed on " + LocalDateTime.now());
     }
 }
--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -3,10 +3,6 @@ package com.example.demo.rest;
 import com.example.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class DemoController {
     private Coach myCoach;
-    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(Coach theCoach, Coach theAnotherCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println(getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach=theAnotherCoach;
-    }
-
-    @GetMapping("/check")
-    public String checkCoach(){
-        return ("myCoach and anotherCoach bean instances are same : "+(myCoach==anotherCoach));
     }
 
     @GetMapping("/dailyworkout")
