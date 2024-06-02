5a2ebb4806f0471b33479e389b4c20325b3a20a7
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-06-02 12:04:40 +0530

    bean scope singleton vs prototypr scope, make sure to include separate coach instances to see the differences

--- src/main/java/com/example/demo/common/TrackCoach.java
+++ src/main/java/com/example/demo/common/TrackCoach.java
@@ -1,10 +1,13 @@
 package com.example.demo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TrackCoach implements Coach {
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -3,17 +3,29 @@ package com.example.demo.rest;
 import com.example.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(Coach theCoach, Coach theAnotherCoach) {
         System.out.println(getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach=theAnotherCoach;
+    }
+
+    @GetMapping("/check")
+    public String checkCoach(){
+        return ("myCoach and anotherCoach bean instances are same : "+(myCoach==anotherCoach));
     }
 
     @GetMapping("/dailyworkout")
